805e3f573c3ac925d9e770eb9ce0f6ada263a3a0
choidz <tjdgusxx@gmail.com>
2024-11-24 02:22:48 +0900

    Feat : pomodoro Feat : home_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,154 +1,175 @@
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
-import 'package:toonflix/widget/button.dart';
-import 'package:toonflix/widget/currrency_card.dart';
+import 'package:toonflix/screen/home_screen.dart';
 
-void main() {
-  runApp(const App());
-}
+// void main() {
+//   runApp(const App());
+// }
 
-// flutter SDK 에 있는 3개의 core Widget 중 하나를 상소ㄱ 받아야한다.
-// st라고 입력하면 몇개의 state가 뜸 그걸말하는거였나..
-class App extends StatelessWidget {
-  const App({super.key});
+// class App extends StatelessWidget {
+//   const App({super.key});
 
-  // 모든 Widget은 build 메소드를 구현해야한다.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData(
+//         textTheme: const TextTheme(
+//           displayLarge: TextStyle(
+//             color: Color(0xFF232B55),
+//           ),
+//         ),
+//         cardColor: const Color(0xFFF4EDDB),
+//         colorScheme: const ColorScheme(surface: Color(0xFFE7626C)),
+//       ),
+//       home: const HomeScreen(),
+//     );
+//   }
+// }
 
-  @override // auto complete 됨.
-  Widget build(BuildContext context) {
-    // material(구글) 또는 cupertino(ios) 위젯을 return
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color(0xFF181818),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 80,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        const Text(
-                          'Hey, Selena',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 28,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                        Text(
-                          'Welcome back',
-                          style: TextStyle(
-                            color: Colors.white.withOpacity(0.5),
-                            fontSize: 18,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 70,
-                ),
-                Text(
-                  "Total Balance",
-                  style: TextStyle(
-                    fontSize: 22,
-                    color: Colors.white.withOpacity(0.8),
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Text(
-                  "\$5 194 382",
-                  style: TextStyle(
-                    fontSize: 44,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
-                  ),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Button(
-                      text: 'Transfer',
-                      bgColor: Color(0xFFF1B33B),
-                      textColor: Colors.black,
-                    ),
-                    Button(
-                      text: 'Request',
-                      bgColor: Color(0xFF1F2123),
-                      textColor: Colors.white,
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 60,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      "Wallets",
-                      style: TextStyle(
-                        fontSize: 35,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Text(
-                      "View All",
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white.withOpacity(0.5),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const CurrencyCard(
-                  name: 'Euro',
-                  code: 'EUR',
-                  amount: '6 428',
-                  icon: Icons.euro_rounded,
-                  isInverted: false,
-                  offset: 0,
-                ),
-                const CurrencyCard(
-                  name: 'Bitcoin',
-                  code: 'BTC',
-                  amount: '9 785',
-                  icon: Icons.currency_bitcoin,
-                  isInverted: true,
-                  offset: -20,
-                ),
-                const CurrencyCard(
-                  name: 'Dollar',
-                  code: 'USD',
-                  amount: '428',
-                  icon: Icons.attach_money_outlined,
-                  isInverted: false,
-                  offset: -40,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+// // flutter SDK 에 있는 3개의 core Widget 중 하나를 상소ㄱ 받아야한다.
+// // st라고 입력하면 몇개의 state가 뜸 그걸말하는거였나..
+// class App extends StatelessWidget {
+//   const App({super.key});
+
+//   // 모든 Widget은 build 메소드를 구현해야한다.
+
+//   @override // auto complete 됨.
+//   Widget build(BuildContext context) {
+//     // material(구글) 또는 cupertino(ios) 위젯을 return
+//     return MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: const Color(0xFF181818),
+//         body: SingleChildScrollView(
+//           child: Padding(
+//             padding: const EdgeInsets.symmetric(
+//               horizontal: 20,
+//             ),
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 const SizedBox(
+//                   height: 80,
+//                 ),
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.end,
+//                   children: [
+//                     Column(
+//                       crossAxisAlignment: CrossAxisAlignment.end,
+//                       children: [
+//                         const Text(
+//                           'Hey, Selena',
+//                           style: TextStyle(
+//                             color: Colors.white,
+//                             fontSize: 28,
+//                             fontWeight: FontWeight.w700,
+//                           ),
+//                         ),
+//                         Text(
+//                           'Welcome back',
+//                           style: TextStyle(
+//                             color: Colors.white.withOpacity(0.5),
+//                             fontSize: 18,
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                   ],
+//                 ),
+//                 const SizedBox(
+//                   height: 70,
+//                 ),
+//                 Text(
+//                   "Total Balance",
+//                   style: TextStyle(
+//                     fontSize: 22,
+//                     color: Colors.white.withOpacity(0.8),
+//                   ),
+//                 ),
+//                 const SizedBox(
+//                   height: 10,
+//                 ),
+//                 const Text(
+//                   "\$5 194 382",
+//                   style: TextStyle(
+//                     fontSize: 44,
+//                     fontWeight: FontWeight.w600,
+//                     color: Colors.white,
+//                   ),
+//                 ),
+//                 const SizedBox(
+//                   height: 25,
+//                 ),
+//                 const Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                   children: [
+//                     Button(
+//                       text: 'Transfer',
+//                       bgColor: Color(0xFFF1B33B),
+//                       textColor: Colors.black,
+//                     ),
+//                     Button(
+//                       text: 'Request',
+//                       bgColor: Color(0xFF1F2123),
+//                       textColor: Colors.white,
+//                     ),
+//                   ],
+//                 ),
+//                 const SizedBox(
+//                   height: 60,
+//                 ),
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                   children: [
+//                     const Text(
+//                       "Wallets",
+//                       style: TextStyle(
+//                         fontSize: 35,
+//                         fontWeight: FontWeight.bold,
+//                         color: Colors.white,
+//                       ),
+//                     ),
+//                     Text(
+//                       "View All",
+//                       style: TextStyle(
+//                         fontSize: 20,
+//                         color: Colors.white.withOpacity(0.5),
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//                 const SizedBox(
+//                   height: 20,
+//                 ),
+//                 const CurrencyCard(
+//                   name: 'Euro',
+//                   code: 'EUR',
+//                   amount: '6 428',
+//                   icon: Icons.euro_rounded,
+//                   isInverted: false,
+//                   offset: 0,
+//                 ),
+//                 const CurrencyCard(
+//                   name: 'Bitcoin',
+//                   code: 'BTC',
+//                   amount: '9 785',
+//                   icon: Icons.currency_bitcoin,
+//                   isInverted: true,
+//                   offset: -20,
+//                 ),
+//                 const CurrencyCard(
+//                   name: 'Dollar',
+//                   code: 'USD',
+//                   amount: '428',
+//                   icon: Icons.attach_money_outlined,
+//                   isInverted: false,
+//                   offset: -40,
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
